239d9dc3705eff154a0d732362b126b27a93a4dd
Sagar Hedaoo <sagarhedaoo@gmail.com>
2024-06-25 02:28:23 -0400

    Added Clerk for Login Signup

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,13 +1,24 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { UserButton, useUser } from "@clerk/nextjs";
 
 const Header = () => {
+  const { user, isSignedIn } = useUser();
+
   return (
     <div className="p-5 border-b shadow-sm sticky top-0 z-50 bg-white">
       <div className="flex items-center justify-between">
         <Image src={"/logo.svg"} alt="Logo" width={50} height={25} />
-        <Button>Get Started</Button>
+        {isSignedIn ? (
+          <div className="flex items-center gap-5">
+            <Button variant="outline">Dashboard</Button>
+            <UserButton />
+          </div>
+        ) : (
+          <Button>Get Started</Button>
+        )}
       </div>
     </div>
   );
